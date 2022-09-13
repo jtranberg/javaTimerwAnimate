75616876787226199baa05118ebbdf206c7d965a
jtranberg <35013712+jtranberg@users.noreply.github.com>
2022-09-13 14:44:04 -0700

    first commit

---
+++ java/java.js
@@ -0,0 +1,26 @@
+class Timer {
+    constructor(durationInput, startButton, pauseButton) {
+        this.durationInput = durationInput;
+        this.startButton = startButton;
+        this.pauseButton = pauseButton;
+
+        this.startButton.addEventListener('click', this.start);
+        this.pauseButton.addEventListener('click', this.pause);        
+    }
+
+    start = () => {
+      this.tick();
+      this.interval = setInterval(this.tick, 1000);
+    };
+pause = () => {
+    clearInterval(this.interval);
+};
+tick = () => {
+    const timeRemaining = parseFloat(this.durationInput.value);
+    this.durationInput = timeRemaining - 1;
+};
+}
+
+const durationInput = document.querySelector('#duration');
+const startButton = document.querySelector("#start");
+const pausetButton = document.querySelector("#pause");
