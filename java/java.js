b199e12e62cbc9d0460b9f4183b8f2f68d755d03
jtranberg <35013712+jtranberg@users.noreply.github.com>
2022-09-13 17:33:45 -0700

    second commit, timer , works well , no CSS,

--- java/java.js
+++ java/java.js
@@ -1,26 +1,33 @@
-class Timer {
-    constructor(durationInput, startButton, pauseButton) {
-        this.durationInput = durationInput;
+class Timer {                                                //createa class
+    constructor(durationInput, startButton, pauseButton) {   //constructor called once
+        this.durationInput = durationInput;                  //this. reffers to class
         this.startButton = startButton;
         this.pauseButton = pauseButton;
 
-        this.startButton.addEventListener('click', this.start);
+        this.startButton.addEventListener('click', this.start);  //add action
         this.pauseButton.addEventListener('click', this.pause);        
     }
 
-    start = () => {
-      this.tick();
-      this.interval = setInterval(this.tick, 1000);
+    start = () => {                                              //method for start button
+     this.tick();                                                //this call
+     this.interval = setInterval(this.tick, 1000);               //interval
+     console.log('start');
     };
-pause = () => {
-    clearInterval(this.interval);
-};
-tick = () => {
+  
+   pause = () => {                                               //pause method
+      clearInterval(this.interval);                              // reset input to 0
+      console.log('pasue');
+    };
+    tick = () => {                                               // tick.. is counter
+        console.log('tick');
     const timeRemaining = parseFloat(this.durationInput.value);
-    this.durationInput = timeRemaining - 1;
-};
+    this.durationInput.value = timeRemaining - 1;                //creates countdown
+    };
 }
 
-const durationInput = document.querySelector('#duration');
-const startButton = document.querySelector("#start");
-const pausetButton = document.querySelector("#pause");
+ const durationInput = document.querySelector('#duration');       //variables
+ const startButton = document.querySelector('#start');
+ const pausetButton = document.querySelector('#pause');
+ const timer = new Timer(durationInput, startButton, pausetButton); //calls new instance of timer
+
+
